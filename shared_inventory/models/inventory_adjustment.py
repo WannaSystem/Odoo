e0492243b215d02699dcb2be8ff0112c9a4e5b32
Ana-Gabriel <agp@odoo.com>
2020-02-26 14:58:33 -0800

    [ADD] shared_inventory: inherit product template methods

--- shared_inventory/models/inventory_adjustment.py
+++ shared_inventory/models/inventory_adjustment.py
@@ -4,23 +4,5 @@ from odoo import api, models, fields
 class InventoryAdjustment(models.Model):
     _inherit = 'stock.inventory'
 
-
-
 class InventoryLine(models.Model):
     _inherit = 'stock.inventory.line'
-
-    @api.one
-    @api.depends('location_id', 'product_id', 'package_id', 'product_uom_id', 'company_id', 'prod_lot_id', 'partner_id')
-    def _compute_theoretical_qty(self):
-        if not self.product_id:
-            self.theoretical_qty = 0
-            return
-        theoretical_qty = self.product_id.get_theoretical_quantity(
-            self.product_id.id,
-            self.location_id.id,
-            lot_id=self.prod_lot_id.id,
-            package_id=self.package_id.id,
-            owner_id=self.partner_id.id,
-            to_uom=self.product_uom_id.id,
-        )
-        self.theoretical_qty = theoretical_qty
