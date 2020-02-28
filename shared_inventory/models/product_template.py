a2756c5bb2ae3fb85138c4ad016510008981261d
Ana-Gabriel <agp@odoo.com>
2020-02-28 15:25:17 -0800

    [REF] shared_inventory: cleaned up code

--- shared_inventory/models/product_template.py
+++ shared_inventory/models/product_template.py
@@ -104,17 +104,17 @@ class ProductTemplate(models.Model):
 
             counts = sku._compute_product_quantities(lot_id, owner_id, package_id, from_date, to_date)
             total_qty[temp.id] = {
-                'qty_available': counts[sku.id]['qty_available'],
-                'virtual_available': counts[sku.id]['virtual_available'],
                 'incoming_qty': counts[sku.id]['incoming_qty'],
-                'outgoing_qty': counts[sku.id]['outgoing_qty']
+                'outgoing_qty': counts[sku.id]['outgoing_qty'],
+                'qty_available': counts[sku.id]['qty_available'],
+                'virtual_available': counts[sku.id]['virtual_available']
             }
 
             for p in sku.sub_product_ids:
                 counts = p._compute_product_quantities(lot_id, owner_id, package_id, from_date, to_date)
-                total_qty[temp.id]['qty_available'] += counts[p.id]['qty_available']
-                total_qty[temp.id]['virtual_available'] += counts[p.id]['virtual_available']
                 total_qty[temp.id]['incoming_qty'] += counts[p.id]['incoming_qty']
                 total_qty[temp.id]['outgoing_qty'] += counts[p.id]['outgoing_qty']
+                total_qty[temp.id]['qty_available'] += counts[p.id]['qty_available']
+                total_qty[temp.id]['virtual_available'] += counts[p.id]['virtual_available']
 
         return total_qty
