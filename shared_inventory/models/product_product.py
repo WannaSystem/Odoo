c94f1405b67c2d1e620074d5f4cb38744eb500f9
Ana-Gabriel <agp@odoo.com>
2020-03-02 12:50:50 -0800

    [FIX] shared_inventory: call compute on product template

--- shared_inventory/models/product_product.py
+++ shared_inventory/models/product_product.py
@@ -10,7 +10,7 @@ class ProductProduct(models.Model):
         for temp in self:
             sku = temp.sku_id if temp.sku_id else temp
 
-            counts = sku._compute_product_quantities(lot_id, owner_id, package_id, from_date, to_date)
+            counts = sku.product_tmpl_id._compute_product_quantities(lot_id, owner_id, package_id, from_date, to_date)
             total_qty[temp.id] = {
                 'incoming_qty': counts[sku.id]['incoming_qty'],
                 'outgoing_qty': counts[sku.id]['outgoing_qty'],
@@ -19,7 +19,7 @@ class ProductProduct(models.Model):
             }
 
             for p in sku.sub_product_ids:
-                counts = p._compute_product_quantities(lot_id, owner_id, package_id, from_date, to_date)
+                counts = p.product_tmpl_id._compute_product_quantities(lot_id, owner_id, package_id, from_date, to_date)
                 total_qty[temp.id]['incoming_qty'] += counts[p.id]['incoming_qty']
                 total_qty[temp.id]['outgoing_qty'] += counts[p.id]['outgoing_qty']
                 total_qty[temp.id]['qty_available'] += counts[p.id]['qty_available']
