dbcfc94896a5eab880350ba67e3d08472d2b1bfb
Ana-Gabriel <agp@odoo.com>
2020-03-02 16:24:00 -0800

    [FIX] shared_inventory: stock move validation

--- shared_inventory/models/__init__.py
+++ shared_inventory/models/__init__.py
@@ -5,4 +5,5 @@ from . import product_product
 from . import inventory_adjustment
 from . import sale_order
 from . import stock_move
+from . import stock_picking
 from . import stock_scrap
--- shared_inventory/models/product_product.py
+++ shared_inventory/models/product_product.py
@@ -5,24 +5,3 @@ from odoo.tools.float_utils import float_round
 class ProductProduct(models.Model):
     _inherit = 'product.product'
     
-    # def _compute_quantities_dict(self, lot_id=False, owner_id=False, package_id=False, from_date=False, to_date=False):
-    #     total_qty = {}
-    #     for temp in self:
-    #         sku = temp.sku_id if temp.sku_id else temp
-
-    #         counts = sku._compute_product_quantities(lot_id, owner_id, package_id, from_date, to_date)
-    #         total_qty[temp.id] = {
-    #             'incoming_qty': counts[sku.id]['incoming_qty'],
-    #             'outgoing_qty': counts[sku.id]['outgoing_qty'],
-    #             'qty_available': counts[sku.id]['qty_available'],
-    #             'virtual_available': counts[sku.id]['virtual_available']
-    #         }
-
-    #         for p in sku.sub_product_ids:
-    #             counts = p._compute_product_quantities(lot_id, owner_id, package_id, from_date, to_date)
-    #             total_qty[temp.id]['incoming_qty'] += counts[p.id]['incoming_qty']
-    #             total_qty[temp.id]['outgoing_qty'] += counts[p.id]['outgoing_qty']
-    #             total_qty[temp.id]['qty_available'] += counts[p.id]['qty_available']
-    #             total_qty[temp.id]['virtual_available'] += counts[p.id]['virtual_available']
-
-    #     return total_qty
