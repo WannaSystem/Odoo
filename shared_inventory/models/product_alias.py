f75c3a33c70cfc4892944977dd59c267d689fb03
Ana-Gabriel <agp@odoo.com>
2020-03-05 13:34:26 -0800

    [ADD] shared_inventory: product_template

--- shared_inventory/models/product_alias.py
+++ shared_inventory/models/product_alias.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from odoo import api, models, fields
-from odoo.tools.float_utils import float_round
 
 class ProductAlias(models.Model):
     _inherit = 'product.alias'
--- shared_inventory/models/product_template.py
+++ shared_inventory/models/product_template.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from odoo import api, models, fields
-from odoo.tools.float_utils import float_round
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
