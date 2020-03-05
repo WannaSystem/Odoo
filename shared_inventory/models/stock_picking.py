62e1af1515fb4de311bcb32ee175522b4c15b782
Ana-Gabriel <agp@odoo.com>
2020-03-05 13:26:37 -0800

    [ADD] shared_inventory: remove sub products and add tags instead

--- shared_inventory/models/stock_picking.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, models, fields
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
--- shared_inventory/models/stock_scrap.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, models, fields
-
-class StockScrap(models.Model):
-    _inherit = 'stock.scrap'
