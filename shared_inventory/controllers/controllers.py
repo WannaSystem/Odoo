fc12bc2b32714da95908d8ca1b032d866fe46be2
Ana-Gabriel <agp@odoo.com>
2020-03-05 17:46:40 -0800

    [ADD] shared_inventory: expression parser to insert expression

---
+++ shared_inventory/controllers/controllers.py
@@ -0,0 +1,7 @@
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+class WebsiteSale(WebsiteSale):
+    def _get_search_domain(self, search, category, attrib_values, search_in_description=True):
+        return super(WebsiteSale, self)._get_search_domain(search, category, attrib_values, search_in_description)
