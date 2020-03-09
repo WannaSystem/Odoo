8f8844c08183ecd8699b2a51165fdd8ccb7c232e
Ana-Gabriel <agp@odoo.com>
2020-03-09 14:56:19 -0700

    [FIX] shared_inventory: insert function bug

--- shared_inventory/models/product_search.py
+++ shared_inventory/models/product_search.py
@@ -25,11 +25,17 @@ def find_expression(key=None, array=[]):
     return result
 
 def insert_expression(array=[], level=0, after=0, operator='|', expr=None):
+        
+    result = []
+    if len(array) == 0:
+        result.append(expr)
+        return result
     
     n = -1
     count = -1
     tree = []
 
+    inserted = False
     for a in array:
         if count == level and n == after: 
             tree.insert(count-n, [operator])
@@ -39,6 +45,7 @@ def insert_expression(array=[], level=0, after=0, operator='|', expr=None):
             
             n += 2
             tree[count].append(a)
+            inserted = True
         elif '&' in a or '|' in a:
             n = -1
             count += 1
@@ -49,14 +56,48 @@ def insert_expression(array=[], level=0, after=0, operator='|', expr=None):
                 count += 1
                 tree.append([])
             tree[count].append(a)
+    
+    if not inserted: 
+        tree.insert(level-after, [operator])
+        tree[level+1].insert(after+1, expr)
 
-    result = []
     for t in tree:
         for e in t:
             result.append(e)
     
     return result
 
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+    
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        
+        if args is None:
+            args = []
+
+        found = find_expression('product_id', args)
+        check = find_expression('product_id.internal_ref_ids.name', args)
+
+        if found['level'] > -1 and check['level'] < 0:
+            args = insert_expression(args, found['level'], found['index'], '|', ['product_id.internal_ref_ids.name', 'ilike', found['value'][2]])
+
+        return super(StockMoveLine, self)._search(args=args, offset=offset, order=order, count=count, access_rights_uid=access_rights_uid)
+    
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        
+        if args is None:
+            args = []
+        
+        found = find_expression('product_id', args)
+        check = find_expression('product_id.internal_ref_ids.name', args)
+
+        if found['level'] > -1 and check['level'] < 0:
+            args = insert_expression(args, found['level'], found['index'], '|', ['product_id.internal_ref_ids.name', 'ilike', name])
+
+        return super(StockMoveLine, self)._name_search(name=name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid)
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -85,9 +126,7 @@ class ProductTemplate(models.Model):
         if found['level'] > -1:
             args = insert_expression(args, found['level'], found['index'], '|', ['internal_ref_ids.name', 'ilike', name])
 
-        return super(ProductTemplate, self)._name_search(
-            name=name, args=args,
-            operator=operator, limit=limit, name_get_uid=name_get_uid)
+        return super(ProductTemplate, self)._name_search(name=name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid)
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
@@ -116,6 +155,4 @@ class ProductProduct(models.Model):
         if found['level'] > -1:
             args = insert_expression(args, found['level'], found['index'], '|', ['internal_ref_ids.name', 'ilike', name])
 
-        return super(ProductProduct, self)._name_search(
-            name=name, args=args,
-            operator=operator, limit=limit, name_get_uid=name_get_uid)
+        return super(ProductProduct, self)._name_search(name=name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid)
