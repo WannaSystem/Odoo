62b8dd638d025f01675f20adbd430a7515c549bb
Ana-Gabriel <agp@odoo.com>
2020-03-12 09:33:26 -0700

    [REF] shared_inventory: remove print statements

--- shared_inventory/controllers/controllers.py
+++ shared_inventory/controllers/controllers.py
@@ -4,5 +4,4 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class WebsiteSale(WebsiteSale):
     def _get_search_domain(self, search, category, attrib_values, search_in_description=True):
-        print(search)
         return super(WebsiteSale, self)._get_search_domain(search, category, attrib_values, search_in_description)
